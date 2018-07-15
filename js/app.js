2a9586c24c0f41d9175d685262c1a56c808cc9ab
priscilafernandes <pris1@outlook.com.br>
2018-07-15 17:11:30 -0300

    total de alunas

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ classYearFilter.addEventListener('change', showDataScreen);
 
 window.onload = loadFilters();
 
-function loadFilters(/*itemCity, itemClassYear*/) {
+// função para carregar filtros das sedes e turmas
+function loadFilters() {
 	var selectCity = document.createElement('option');
 	selectCity.innerHTML = 'Escolha a Sede';
 	selectCity.value = 'none';
@@ -15,13 +16,9 @@ function loadFilters(/*itemCity, itemClassYear*/) {
 		itemFilter.value = city;
 		itemFilter.innerHTML = city;
 		cityFilter.appendChild(itemFilter);
-	}
+	}		
 
-	var selectClassYear = document.createElement('option');
-		selectClassYear.innerHTML = '';
-		selectClassYear.value = 'none';
-		classYearFilter.appendChild(selectClassYear);
-
+	// ao esolher a sede, mostra as turmas correspondentes a ela 
 	cityFilter.addEventListener('change', function (event) {	
 		if (event.target.value === 'AQP') {
 			var itemClassYear = data.AQP;
@@ -37,6 +34,11 @@ function loadFilters(/*itemCity, itemClassYear*/) {
     		classYearFilter.removeChild(classYearFilter.firstChild);
 		}
 
+		var selectClassYear = document.createElement('option');
+		selectClassYear.innerHTML = 'Escolha a Turma';
+		selectClassYear.value = 'none';
+		classYearFilter.appendChild(selectClassYear);
+
 		for (item in itemClassYear) {
 			var itemFilter = document.createElement('option');
 			itemFilter.value = item;
@@ -46,19 +48,49 @@ function loadFilters(/*itemCity, itemClassYear*/) {
 	});
 }
 
-function showDataScreen(){
+// retornam as turmas de cada sede
+var AQPcity = data['AQP'];
+var CDMXcity = data['CDMX'];
+var LIMcity = data['LIM'];
+var SCLcity = data['SCL'];
+
+// retornam os objetos dentro de cada turma
+var AQPclass1 = data['AQP']['2016-2'];
+var AQPclass2 = data['AQP']['2017-1'];
+
+var CDMXclass1 = data['CDMX']['2017-1'];
+var CDMXclass2 = data['CDMX']['2017-2'];
+
+var LIMclass1 = data['LIM']['2016-2'];
+var LIMclass2 = data['LIM']['2017-1'];
+var LIMclass3 = data['LIM']['2017-2'];
+
+var SCLclass1 = data['SCL']['2016-2'];
+var SCLclass2 = data['SCL']['2017-1'];
+var SCLclass3 = data['SCL']['2017-2'];
+
+// função para mostrar resultados na tela
+function showDataScreen() {
 	var city = cityFilter.value;
 	var classYear = classYearFilter.value;
 
 	var showData = document.getElementById('show-data');
 	showData.innerHTML = '';
 
-	// for (option in data[city]) {
-	// 	for (i in data[city][option]['students']) {			
-	// 		var result = document.createElement('p');
-	// 		showData.appendChild(result);
-	// 	}
-	// }
+	classYearFilter.addEventListener('change', function(event) {	
+		if (cityFilter.value === 'AQP' && event.target.value === Object.keys(AQPcity)[0]) {
+			return button1AQPclass1();	
+		}
+	});
+
+	// função para resultado dos dados gerais da turma AQPclass1
+	function button1AQPclass1() {
+		var students = document.createTextNode(AQPclass1['students'].length + ' alunas'); 
+		var paragraph = document.createElement('p');
+		paragraph.className = 'result';
+		showData.appendChild(paragraph);
+		paragraph.appendChild(students); 
+	}
 }
 
 console.log(data);
